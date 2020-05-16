51db09a50d3e140a7df5c0b9467b2cd7a37d74d2
Enyarina <enrina050220005@gmail.com>
2020-05-16 13:32:43 +0300

    need

---
+++ cmake-build-debug/CMakeFiles/7_lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/7_lab.dir/sources/source.cpp.o"
+  "7_lab.pdb"
+  "7_lab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
